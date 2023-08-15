a212b2f95d1c4181b54c4be433967e8fc7a8a7c7
AshCatchEmAll <avichare@sfu.ca>
2023-08-15 15:55:26 -0700

    Submission For hashnode

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDKaEIK4PZ3nTaCqiO5dxdNysVaWPOwjEk")
+    GMSServices.provideAPIKey("YOUR GOOGLE API KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
